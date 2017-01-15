06382907c2efddbd93e90873a7ea884a59d394b8
Mohsen MahmoudAbadi <speitk3en@gmail.com>
2017-01-15 20:06:31 +0330

    Update Router.php

--- library/Router.php
+++ library/Router.php
@@ -1,64 +1,61 @@
 <?php
+ 
+class Router {
+	static private $controller,$action,$param;
+	static private $url;
+	static public $IsRoute;
 
-class Router
-{
-    static private $controller, $action, $param;
-    static private $url;
-    static public $IsRoute;
 
-
-    static function CheckRoute()
+	static function CheckRoute()
     {
-        $controller = self::$controller . _Control;
+        $controller = self::$controller._Control;
 
-        echo ROOT_PATH . DS . 'controller' . DS . $controller . '.php <br>';
+        echo ROOT_PATH.DS.'controller'.DS.$controller.'.php <br>';
 
-        if (file_exists(ROOT_PATH . DS . 'controller' . DS . $controller . '.php')) {
+        if (file_exists(ROOT_PATH.DS.'controller'.DS.$controller.'.php')) {
             $controllerObj = new $controller();
 
-            if (method_exists($controllerObj, self::$action)) {
+            if(method_exists($controllerObj, self::$action)){
                 call_user_func_array(
-                    [$controllerObj, self::$action],
+                    [$controllerObj,self::$action],
                     self::$param ?? []
                 );
                 self::$IsRoute = 1;
-            } else {
-                die("Action " . self::$action . " not found in $controller Class");
+            }else{
+                die("Action ".self::$action." not found in $controller Class");
             }
 
         }
     }
 
 
-<<<<<<< HEAD
 
 
 
     static function post($url = '',$params = '')
-=======
-    static function post($url = '', $params = '')
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
     {
 
-        if (empty($url) && empty($params) && DefaultRouter == 1) {
+        if(empty($url) && empty($params) && DefaultRouter == 1)
+        {
             self::defaultRout("post");
             self::CheckRoute();
-        } else {
+        }
+        else
+        {
             $geturl = @$_POST['url'];
 
-            if (strpos($url, "/$")) {
+            if(strpos($url,"/$")) {
 
-                $parm = self::Exp("/", $geturl);
+                $parm = self::Exp("/",$geturl);
                 $count = count($parm);
-                self::$param = [$parm[$count - 1]];
-                unset($parm[$count - 1]);
-                $geturl = implode("/", $parm);
+                self::$param = [$parm[$count-1]];
+                unset($parm[$count-1]);
+                $geturl = implode("/",$parm);
 
 
                 $url = self::ChangeUrl($url);
 
             }
-<<<<<<< HEAD
             if("/".$geturl == $url)
             {
                 if(is_callable($params))
@@ -67,13 +64,6 @@ class Router
                     call_user_func_array($params,[]);
                 }
                 else {
-=======
-            if ("/" . $geturl == $url) {
-                if (is_callable($params)) {
-                    self::$IsRoute = 1;
-                    return $params();
-                } else {
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
                     $get = self::Exp("@", $params);
 
                     self::$controller = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
@@ -89,30 +79,27 @@ class Router
     }
 
 
-    static public function get($url = '', $params = '')
+
+    static public function get($url = '',$params = '')
     {
-        if (empty($url) && empty($params) && DefaultRouter == 1) {
+        if(empty($url) && empty($params) && DefaultRouter == 1)
+        {
             self::defaultRout();
             self::CheckRoute();
-        } else {
+        }
+        else
+        {
             $geturl = $_GET['url'];
 
-<<<<<<< HEAD
             if(strpos($url,"/$")) {
                 $parm = self::Exp("/",$geturl);
-=======
-            if (strpos($url, "/$")) {
-
-                $parm = self::Exp("/", $geturl);
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
                 $count = count($parm);
-                self::$param = [$parm[$count - 1]];
-                unset($parm[$count - 1]);
-                $geturl = implode("/", $parm);
+                self::$param = [$parm[$count-1]];
+                unset($parm[$count-1]);
+                $geturl = implode("/",$parm);
 
                 $url = self::ChangeUrl($url);
             }
-<<<<<<< HEAD
             if("/".$geturl == $url)
             {
                 if(is_callable($params))
@@ -122,29 +109,14 @@ class Router
                     call_user_func_array($params,[]);
                 }
                 else {
-=======
-            if ("/" . $geturl == $url) {
-                if (is_callable($params)) {
-                    self::$IsRoute = 1;
-                    return $params();
-                } else {
+
                     $get = self::Exp("@", $params);
 
                     self::$controller = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
                     array_shift($get);
                     self::$action = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
-
-                    $get = self::Exp("@", $params);
-
-<<<<<<< HEAD
-                    self::$controller = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
-                    array_shift($get);
-                    self::$action = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
 
 
-=======
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
                     self::CheckRoute();
                 }
             }
@@ -153,29 +125,30 @@ class Router
     }
 
 
-    static function route($url = '', $params = '')
-    {
 
+    static function route($url = '',$params = ''){
 
-        if (empty($url) && empty($params) && DefaultRouter == 1) {
+        if(empty($url) && empty($params) && DefaultRouter == 1)
+        {
             self::defaultRout();
             self::CheckRoute();
-        } else {
+        }
+        else
+        {
             $geturl = $_REQUEST['url'];
 
-            if (strpos($url, "/$")) {
+            if(strpos($url,"/$")) {
 
-                $parm = self::Exp("/", $geturl);
+                $parm = self::Exp("/",$geturl);
                 $count = count($parm);
-                self::$param = [$parm[$count - 1]];
-                unset($parm[$count - 1]);
-                $geturl = implode("/", $parm);
+                self::$param = [$parm[$count-1]];
+                unset($parm[$count-1]);
+                $geturl = implode("/",$parm);
 
 
                 $url = self::ChangeUrl($url);
 
             }
-<<<<<<< HEAD
             if("/".$geturl == $url)
             {
                 if(is_callable($params))
@@ -186,74 +159,63 @@ class Router
                 }
                 else{
                 $get = self::Exp("@",$params);
-=======
-            if ("/" . $geturl == $url) {
-                if (is_callable($params)) {
-                    self::$IsRoute = 1;
-                    return $params();
-                } else {
-                    $get = self::Exp("@", $params);
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
 
-                    self::$controller = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
-                    array_shift($get);
-                    self::$action = Helper::Definelow(($get[0] == '') ? 'index' : $get[0]);
+                self::$controller = Helper::Definelow(($get[0] == '' ) ? 'index' : $get[0]);
+                array_shift($get);
+                self::$action = Helper::Definelow(($get[0] == '' ) ? 'index' : $get[0]);
 
 
-<<<<<<< HEAD
                 self::CheckRoute();
-=======
-                    self::CheckRoute();
->>>>>>> eaf074becc19bc90a287f32a6bad2c5bfaf4b6e5
                 }
             }
-
-
         }
+
+
 
     }
 
-
-    static private function Exp($tag, $str)
+    static private function Exp($tag,$str)
     {
-        return explode($tag, $str);
+        return explode($tag,$str);
     }
 
     static private function defaultRout($method = 'Request')
     {
-        switch ($method) {
+        switch ($method)
+        {
             case "get":
-                $urlparts = self::Exp("/", $_GET['url']);
+                $urlparts = self::Exp("/",$_GET['url']);
                 break;
             case "post":
-                $urlparts = self::Exp("/", $_POST['url']);
+                $urlparts = self::Exp("/",$_POST['url']);
                 break;
 
             default:
-                $urlparts = self::Exp("/", $_REQUEST['url']);
+                $urlparts = self::Exp("/",$_REQUEST['url']);
                 break;
         }
 
 
-        self::$controller = Helper::Definelow(($urlparts[0] == '') ? 'index' : $urlparts[0]);
+
+        self::$controller = Helper::Definelow(($urlparts[0] == '' ) ? 'index' : $urlparts[0]);
         array_shift($urlparts);
-        self::$action = Helper::Definelow(($urlparts[0] == '') ? 'index' : $urlparts[0]);
+        self::$action = Helper::Definelow(($urlparts[0] == '' ) ? 'index' : $urlparts[0]);
         array_shift($urlparts);
-        self::$param = ($urlparts == '') ? '' : $urlparts;
+        self::$param = ($urlparts == '')? '' : $urlparts ;
 
     }
 
     static public function ChangeUrl($url)
     {
-        $parm = self::Exp("/", $url);
+        $parm = self::Exp("/",$url);
         $count = count($parm);
-        unset($parm[$count - 1]);
-        return implode("/", $parm);
+            unset($parm[$count-1]);
+        return implode("/",$parm);
     }
-
     static public function Run($redirect = "404")
     {
-        if (self::$IsRoute != 1) {
+        if(self::$IsRoute != 1)
+        {
             Load::viewer($redirect);
         }
     }
